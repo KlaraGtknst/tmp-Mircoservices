0ecfaf2ad8877a83cca907f55f104fdd6bf59fb7
KlaraGtknst <klara.gutekunst.kg@gmail.com>
2022-01-11 16:17:46 +0100

    Improved price validation and resolved copy paste mistakes

--- MicroShop/micro-shop-frontend/src/app/components/edit-offer/edit-offer.component.ts
+++ MicroShop/micro-shop-frontend/src/app/components/edit-offer/edit-offer.component.ts
@@ -17,7 +17,7 @@ export class EditOfferComponent implements OnInit {
 
   formGroup = new FormGroup({
     productName: new FormControl('', [Validators.required, this.productNameValidator()]),
-    productPrice: new FormControl('', [Validators.required, this.productPriceValidator()]),
+    productPrice: new FormControl('', [Validators.required, Validators.pattern("^[0-9]+|^[0-9]*.^[0-9]*$ "), this.productPriceValidator()]),
   });
 
   productNameValidator() {
@@ -29,22 +29,22 @@ export class EditOfferComponent implements OnInit {
 
   productPriceValidator() {
     return (control: AbstractControl): ValidationErrors | null => {
-      const forbidden = control.value <= 0;
+      const forbidden = control.value <= 0; //|| !(!isNaN(parseFloat(control.value)) && isFinite(control.value));
       return forbidden? {forbiddenName: {value: control.value}} : null;
     };
   }
 
-  //WIP
+  /*
   productNameError(control: AbstractControl) {
     const forbidden = this.validNames.indexOf(control.value) < 0;
       return forbidden? {forbiddenName: {value: control.value}} : null;
   }
-  //END WIP
+  */
 
   debugOut = 'Hello edit offer'
 
   submitOffer() {
-    this.debugOut = `Your input is ${this.formGroup.get('productName')?.value} with price: ${this.formGroup.get('productPrice')?.value} `;
+    this.debugOut = `Your input is ${this.formGroup.get('productName')?.value} with price: ${this.formGroup.get('productPrice')?.value}.`;
   }
 
 }
